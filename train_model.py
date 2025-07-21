c0d068a906d6b2c773cd5c30e11159216a18a431
akhilamipara <akhilmamipara2005@gmail.com>
2025-07-21 17:00:37 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,7 +3,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import joblib
 
-# Load dataset
 df = pd.read_csv("heart_failure_clinical_records_dataset.csv")
 
 
@@ -15,6 +14,5 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
-# Save model
 joblib.dump(model, "heart_failure_model.pkl")
 print("✅ Model trained and saved successfully.")
